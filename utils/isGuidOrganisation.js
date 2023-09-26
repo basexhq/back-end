661b1136446d127974030ecf66d62ef6e2a04150
Mars Notahacker <marsXrobertson@gmail.com>
2023-09-26 22:26:45 +0100

    Cleanup

--- utils/isGuidOrganisation.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-async function isGuidOrganisation(BaseXContract, GUID) {
-	const organisationsContract = await BaseXContract.getOrganisations();
-	return organisationsContract.some((org) => org.orgGuid === GUID);
-}
-
-module.exports = isGuidOrganisation;
