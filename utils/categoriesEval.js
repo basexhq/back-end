9d3a08748e7062e595bd677a0d26f8c41711d0cf
Grigore-Gabriel Trifan <55629140+GregTrifan@users.noreply.github.com>
2024-01-13 01:39:05 +0200

    add categories eval be

---
+++ utils/categoriesEval.js
@@ -0,0 +1,12 @@
+const planetaryBoundaries = [
+	"Climate Change",
+	"Biosphere Integrity",
+	"Stratospheric Ozone Depletion",
+	"Land System Change",
+	"Freshwater Use",
+	"Biogeochemical Flows",
+	"Ocean Acidification",
+	"Atmospheric Aerosol Loading",
+];
+
+module.exports = { planetaryBoundaries };
