7d0bf88c1f63c8c4fa3f1782b7a0d580a951042a
Mars Notahacker <marsXrobertson@gmail.com>
2024-02-04 16:39:00 +0000

    Consistent headers and names

--- utils/categoriesEval.js
+++ utils/categoriesEval.js
@@ -1,3 +1,6 @@
+// THIS FILE SHOULD BE EXACTLY THE SAME AS FRONT-END
+// TODO: better sharing of resources between front-end and back-end
+
 const planetaryBoundaries = [
 	"Climate Change",
 	"Biosphere Integrity",
@@ -7,6 +10,57 @@ const planetaryBoundaries = [
 	"Biogeochemical Flows",
 	"Ocean Acidification",
 	"Atmospheric Aerosol Loading",
+	"Novel entities"
 ];
 
-module.exports = { planetaryBoundaries };
+const SDG = [
+	"No Poverty",
+	"Zero Hunger",
+	"Good Health and Well-being",
+	"Quality Education",
+	"Gender Equality",
+	"Clean Water and Sanitation",
+	"Affordable and Clean Energy",
+	"Decent Work and Economic Growth",
+	"Industry, Innovation, and Infrastructure",
+	"Reduced Inequality",
+	"Sustainable Cities and Communities",
+	"Responsible Consumption and Production",
+	"Climate Action",
+	"Life Below Water",
+	"Life on Land",
+	"Peace, Justice, and Strong Institutions",
+	"Partnerships for the Goals",
+]
+
+const EBF = [
+	"Air",
+	"Water",
+	"Soil",
+	"Biodiversity",
+	"Equity",
+	"Carbon"
+]
+
+const unitedPlanet = [
+	"Health & Wellbeing",
+	"Water Security",
+	"Unleashing Potential",
+	"Sustainable Food",
+	"Community Abundance",
+	"Wealthy Commons",
+	"Regenerative Cities",
+	"Instrastructure Transformation",
+	"Gender Harmony",
+	"Peace & Justice",
+	"Thriving for all",
+	"Circular Economies",
+	"Planetary Health",
+	"Ocean Stewardship",
+	"Ubiquitous Energy",
+	"Land Stewardship"
+]
+
+
+
+module.exports = { planetaryBoundaries, SDG, EBF, unitedPlanet };
