0ca2644040a21fd7e32266fef69be88e0bc44be9
Grigore Gabriel Trifan <trifangaby71@protonmail.com>
2024-05-17 20:29:01 +0300

    bump api version

--- controllers/stripeController.mjs
+++ controllers/stripeController.mjs
@@ -10,7 +10,7 @@ import Mailgun from "mailgun.js";
 env.config({ path: "./.env" });
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-	apiVersion: "2023-10-16",
+	apiVersion: "2024-04-10",
 	appInfo: {
 		// For sample support and debugging, not required for production:
 		name: "Genesis RE",
@@ -189,6 +189,7 @@ router.post(
 		if (eventType === "payment_intent.succeeded") {
 			// Cast the event into a PaymentIntent to make use of the types.
 			const pi = data.object;
+
 			// Funds have been captured
 			// Fulfill any orders, e-mail receipts, etc
 			// To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds).
